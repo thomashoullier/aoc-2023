c4a9bdb4bb46ddbb6480e2f2774f8c74d15ce8cd
Thomas HOULLIER <pro@houllier.net>
2023-12-21 17:38:58 +0100

    Day 06 part 2 done

--- day_06/day_06.hs
+++ day_06/day_06.hs
@@ -1,5 +1,6 @@
 -- Day 06
 import Data.List.Split (splitOn)
+import Data.Number.CReal
 
 ex_input = "Time:      7  15   30\n"
         ++ "Distance:  9  40  200\n"
@@ -51,4 +52,54 @@ part1 = do putStrLn "# Part 1 #"
            let ls = lines contents
                races = parseInput ls
                res = countAllWins races
+           print res
+
+-- * Part 2
+-- We now have to solve the problem analytically.
+-- The distance covered is d = h * (t - h)
+-- We need to count the number of d>dlim for h in [0, t]
+-- d = dlim <-> dlim = -h**2 + ht <-> h**2 - ht + dlim = 0
+-- There are two roots h1 and h2, h1 < h2, we just have to count the number of
+-- integral steps between h1 and h2.
+-- a = 1, b = -t, c = dlim
+-- Delta = t**2 - 4 dlim
+-- h1 = (t - sqrt(Delta)) / 2
+-- h2 = (t + sqrt(Delta)) / 2
+-- res = floor(h2) - ceil(h1) + 1
+
+-- See the CReal type, we actually have to use it to avoid Haskell converting to
+-- inexact floats.
+compInterval :: Int -> Int -> Int
+compInterval t d = (floor h2) - (ceiling h1) + 1
+  where delta = t*t - 4 * d
+        h1 = ((fromIntegral t) - ((sqrt :: CReal -> CReal) $ fromIntegral delta)) / (fromIntegral 2)
+        h2 = ((fromIntegral t) + ((sqrt :: CReal -> CReal) $ fromIntegral delta)) / (fromIntegral 2)
+-- ex: compInverval 7 9 -> 4
+
+-- Parse the input in the new fashion
+parseNum :: String -> Int
+parseNum s = read $ concat $ filter (not . (== "")) $ splitOn " " ss
+  where ss = last $ splitOn ":" s
+
+parseInput2 :: [String] -> Race
+parseInput2 ss = Race { time = t, distance = d }
+  where t = parseNum $ head ss
+        d = parseNum $ last ss
+-- parseInput2 $ lines ex_input
+
+ex_bigrace = parseInput2 $ lines ex_input
+
+-- Count the number of different ways to win in a big Race
+countBigWins :: Race -> Int
+countBigWins r = compInterval t d
+  where t = time r
+        d = distance r
+-- ex: countBigWins ex_bigrace
+
+part2 = do putStrLn "# Part 2 #"
+           contents <- readFile "input.txt"
+           let ls = lines contents
+               race = parseInput2 ls
+               res = countBigWins race
+           print race
            print res
